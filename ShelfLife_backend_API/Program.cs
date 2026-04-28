1e3bd1fac35462472c8ecf6d81b33a7d7e7ec12e
Rianne Magtuba <99489243+Rianne-Magtuba@users.noreply.github.com>
2026-04-28 20:05:32 +0800

    fix: add JWT config to prod, mount jwt secret in cloud run

--- ShelfLife_backend_API/Program.cs
+++ ShelfLife_backend_API/Program.cs
@@ -21,6 +21,13 @@ namespace ShellLife_backend_API
             builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
             // Add services to the container.
 
+            const string jwtKeySecretPath = "/secrets/jwt-key";
+            if (File.Exists(jwtKeySecretPath))
+            {
+                var jwtKey = File.ReadAllText(jwtKeySecretPath).Trim();
+                builder.Configuration["Jwt:Key"] = jwtKey;
+            }
+
             builder.Services.AddControllers();
           
 
